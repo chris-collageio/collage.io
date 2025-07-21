d3eba740d9a447f67bb6f18bb74eda1f73719c5d
chris-collageio <collageiopin@gmail.com>
2025-07-21 23:11:16 +0100

    Minor typo in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cors({
   origin: "https://collageio.web.app",
   credentials: true
-}));app.use(express.json());
+}));
+app.use(express.json());
 
 const PORT = 3000;
 
