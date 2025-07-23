23a8afe44ed1d7f868dade7b54699aff312f8834
chris-collageio <collageiopin@gmail.com>
2025-07-23 22:42:50 +0100

    Scale back all changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const qs = require("qs");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: process.env.PINTEREST_REDIRECT_URI }));
+app.use(cors({ origin: "https://collageio.web.app" }));
 app.use(express.json());
 
 app.post("/auth/pinterest/exchange", async (req, res) => {
@@ -33,7 +33,7 @@ app.post("/auth/pinterest/exchange", async (req, res) => {
     // + '&grant_type=authorization_code',
     url: 'https://api.pinterest.com/v5/oauth/token?client_id=1525277&client_secret=dd1fff7697f7afc6cef9d121c5ea8055b13b5245&redirect_uri=https://collageio.web.app/&code=' + code + '&grant_type=authorization_code',
     headers: { 
-      'Authorization': "Basic " + btoa(PINTEREST_CLIENT_ID + ":" + PINTEREST_CLIENT_SECRET), 
+      'Authorization': "Basic MTUyNTI3NzpkZDFmZmY3Njk3ZjdhZmM2Y2VmOWQxMjFjNWVhODA1NWIxM2I1MjQ1", // + btoa(PINTEREST_CLIENT_ID + ":" + PINTEREST_CLIENT_SECRET), 
       'Content-Type': 'application/x-www-form-urlencoded', 
       'Cookie': '_ir=0'
     },
