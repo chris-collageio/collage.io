716b9ae8c9440c1613ae3bdea01adfe354b9d31e
chris-collageio <collageiopin@gmail.com>
2025-07-23 01:08:59 +0100

    Retest of the POSTMAN post request

--- backend/server.js
+++ backend/server.js
@@ -15,30 +15,67 @@ app.post("/auth/pinterest/exchange", async (req, res) => {
     return res.status(400).json({ error: "Missing authorization code" });
   }
 
+  const axios = require('axios');
+  const qs = require('qs');
   let data = qs.stringify({
-    'client_id': process.env.PINTEREST_CLIENT_ID,
-    'client_secret': process.env.PINTEREST_CLIENT_SECRET,
-    'redirect_uri': process.env.PINTEREST_REDIRECT_URI,
-    'code': code,
+    'client_id': '1525277',
+    'client_secret': 'dd1fff7697f7afc6cef9d121c5ea8055b13b5245',
+    'redirect_uri': 'https://collageio.web.app/',
+    'code': '544064a5acafceb3fb897e642ebb4b6837f56655',
     'grant_type': 'authorization_code' 
   });
 
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: 'https://api.pinterest.com/v5/oauth/token?client_id=' + process.env.PINTEREST_CLIENT_ID
-      + '&client_secret=' + process.env.PINTEREST_CLIENT_ID 
-      + '&redirect_uri=https://collageio.web.app/&code=' + code 
-      + '&grant_type=authorization_code',
+    url: 'https://api.pinterest.com/v5/oauth/token?client_id=1525277&client_secret=dd1fff7697f7afc6cef9d121c5ea8055b13b5245&redirect_uri=https://collageio.web.app/&code=544064a5acafceb3fb897e642ebb4b6837f56655&grant_type=authorization_code',
     headers: { 
-      'Authorization': 'Basic' + btoa(process.env.PINTEREST_CLIENT_ID + ':' + process.env.PINTEREST_CLIENT_SECRET), 
+      'Authorization': 'Basic MTUyNTI3NzpkZDFmZmY3Njk3ZjdhZmM2Y2VmOWQxMjFjNWVhODA1NWIxM2I1MjQ1', 
       'Content-Type': 'application/x-www-form-urlencoded', 
       'Cookie': '_ir=0'
     },
     data : data
   };
 
-  axios.request(config);;
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+  // let data = qs.stringify({
+  //   'client_id': process.env.PINTEREST_CLIENT_ID,
+  //   'client_secret': process.env.PINTEREST_CLIENT_SECRET,
+  //   'redirect_uri': process.env.PINTEREST_REDIRECT_URI,
+  //   'code': code,
+  //   'grant_type': 'authorization_code' 
+  // });
+
+  // let config = {
+  //   method: 'post',
+  //   maxBodyLength: Infinity,
+  //   url: 'https://api.pinterest.com/v5/oauth/token?client_id=' + process.env.PINTEREST_CLIENT_ID
+  //     + '&client_secret=' + process.env.PINTEREST_CLIENT_ID 
+  //     + '&redirect_uri=https://collageio.web.app/&code=' + code 
+  //     + '&grant_type=authorization_code',
+  //   headers: { 
+  //     'Authorization': 'Basic' + btoa(process.env.PINTEREST_CLIENT_ID + ':' + process.env.PINTEREST_CLIENT_SECRET), 
+  //     'Content-Type': 'application/x-www-form-urlencoded', 
+  //     'Cookie': '_ir=0'
+  //   },
+  //   data : data
+  // };
+
+  // axios.request(config)
+  // .then((response) => {
+  //   console.log(JSON.stringify(response.data));
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
 
   res.json({ access_token: response.data.access_token });
 
